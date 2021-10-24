1c8ad174bf094056721a25c48c2f5cda516c9bb1
KitFoxy <zewarringfoxo@gmail.com>
2021-10-24 15:07:14 +1100

    Commit

---
+++ headers/audioserver.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <list> //Need to look into a better type.
+
+
+// TCP is the protocol to be used.
+class AudioServer
+{
+public:
+    void start(); // Starts the server
+    void stop(); // Stops the Server
+
+private:
+    ;
+}
---
+++ source/audioserver.cpp
@@ -0,0 +1,13 @@
+#include "audioserver.h"
+
+// TCP is the protocol to be used.
+void AudioServer::start()
+{
+    ;
+}
+
+void AudioServer::stop()
+{
+    ;
+}
+
